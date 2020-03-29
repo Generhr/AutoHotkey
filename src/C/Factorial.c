ecfa2d6bb8450f9f91b7ba804c45dd8dd4155218
Onimuru <Onimuru@gmail.com>
2020-03-29 02:50:01 +0100

    Updated C source code used for MCode in Math.ahk

---
+++ src/C/Factorial.c
@@ -0,0 +1,8 @@
+﻿int function (int n) {
+    int r = 1;
+
+    for (int i = 1; i <= n; ++i)
+        r *= i;
+
+    return r;
+}
---
+++ src/C/Fibonacci.c
@@ -0,0 +1,8 @@
+﻿int function(int n) {
+    int a = 0, b = 1, c;
+
+    for (int i = 1; i <= n; ++i)
+        c = b, b += a, a = c;
+
+    return c;
+}
